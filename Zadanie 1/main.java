b016abb5aad18d01667785c118e08e5180702179
DennetM <DennetM.alt@gmail.com>
2015-11-08 16:24:20 +0100

    Almost finished the Comparator Function
    Almost implemented the kNN algorithm using the Manhattan Metric (or how
    we call it, "metoda uliczna", since it's based off the streets of
    Manhattan).
    
    In essence, I added a way for our KnowledgeGathere to distinguish
    between what sort of database are we filling - the knowledge or the
    matcher.  When we have both databases filled (again, please make sure
    they're both the correct size!), we can invoke the testNumbers(int k)
    function, which in itself is the kNN algorithm, which works as follows:
    > Calculate the distance between the object we're on and EVERY OTHER
    OBJECT IN THE KNOWLEDGEBASE. You do this by summing the absolute values
    of the differences between each individual features (aka sum |x_i -
    y_i|).
    > Sort these from the lowest to highest.
    > Pick k lowest, i.e. the ones closest to our object.
    > Sum up how many times have we encountered each label in that cluster.
    > Decide which label is the most common.
    > Assign that label.
    
    And additionally, I implemented a very simple algorithm at the tail of
    it all.
    > If the label is the same as the original label, increase the success
    rate by 1.
    > Divide the success rate by the matchbase's size.
    > Multiply by 100, add "%" at the end.
    > Enjoy your percentage of correctness (sounds like a D&D item I swear).
    
    "Almost" finished because the arrays gave me a headache. I'll take a
    break and see if I can finish it today, but honestly I'm crapping arrays
    out the side of my skull at the moment.

--- Zadanie 1/main.java
+++ Zadanie 1/main.java
@@ -1,4 +1,3 @@
-import java.awt.event.TextEvent;
 import java.io.IOException;
 
 import ocrKnowledgebase.Knowledge;
@@ -11,16 +10,43 @@ public class main {
 	
 	public static void main(String[] args) throws IOException{
 		
+		
+		//Testing the kNN Algorithm, also the proper program loop.
+		
+		//Size setting.
+		int knowledgeSize = 2500;
+		int matchSize = 100;
+		
+		//Spawn the knowledgebase.
+		Knowledge database = new Knowledge(knowledgeSize, matchSize);
+		//Spawn the extractors
+		DataExtractor extrData = new DataExtractor(knowledgeSize);
+		DataExtractor extrMatch = new DataExtractor(matchSize);
+		
+		//Prepare both extractors.
+		extrData.prepareLabel(ReadDatabase.readLabel("train-labels.idx1-ubyte", knowledgeSize));
+		extrData.prepareImage(ReadDatabase.readImage("train-images.idx3-ubyte", knowledgeSize));
+		
+		extrMatch.prepareLabel(ReadDatabase.readLabel("t10k-labels.idx1-ubyte", matchSize));
+		extrMatch.prepareImage(ReadDatabase.readImage("t10k-images.idx3-ubyte", matchSize));
+		
+		//Gather the knowledge from both extractors.
+		database.gatherKnowledge(extrData, "database");
+		database.gatherKnowledge(extrMatch, "matchbase");
+		
+		//All that done, let's see how effective are we.
+		database.testNumbers(4);
+		
 		/*Success.
 		//Initial read-extract-write test.
 		
-		
 		//Set the core size, which we'll use throughout the document.
 		//IT IS IMPERATIVE EVERYTHING RELATED TO DATA HANDLING IS LIMITED BY THIS NUMBER!!
-		int testSize = 100;
+		int testSize = 1000;
+		int testMatchSize = 0;
 		
 		//Spawn our Knowledgebase.
-		Knowledge testKnowledgebase = new Knowledge(testSize, 0);
+		Knowledge testKnowledgebase = new Knowledge(testSize, testMatchSize);
 		
 		//Spawn our data extractor.
 		DataExtractor testExtractor = new DataExtractor(testSize);
@@ -30,7 +56,7 @@ public class main {
 		testExtractor.prepareImage(ReadDatabase.readImage("train-images.idx3-ubyte", testSize));
 		
 		//Gather the knowledge from the extractor.
-		testKnowledgebase.gatherKnowledge(testExtractor);
+		testKnowledgebase.gatherKnowledge(testExtractor, "database");
 		
 		//Save the knowledge from the database to a file.
 		testKnowledgebase.saveKnowledge("testSave.txt");
--- Zadanie 1/ocrKnowledgebase/Knowledge.java
+++ Zadanie 1/ocrKnowledgebase/Knowledge.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import ocrRecognizer.DataExtractor;
@@ -35,7 +36,7 @@ public class Knowledge {
 		this.size = s;
 		this.sizeMatch = sm;
 		this.knowledgeBase = new double[size][17];
-		this.matchBase = new double[size][17];
+		this.matchBase = new double[sizeMatch][17];
 		
 		//Prepare the arrays, setting _EVERY_ value to 0 just so that we're sure we're not dancing with random values.
 		for(int i=0;i<size;i++){
@@ -53,20 +54,83 @@ public class Knowledge {
 	
 	//Functions
 	//gatherKnowledge - this function will iterate itself through the table, calling our DataExtractor class to fill in the blanks.
-	public void gatherKnowledge(DataExtractor extractor){
-		//Run through everything.
-		for(int i=0; i<size; i++){
-			for(int ft=0; ft<17; ft++){
-				//If our feature is equal zero, i.e. it's the index, gather it.
-				if (ft==0) this.knowledgeBase[i][ft] = extractor.extractLabel(i);
-				//If our feature has an index (1,2,3,4,5,6), extract the feature of that index.
-				else this.knowledgeBase[i][ft] = extractor.extractFeature(i, ft);
-				
-				System.out.println("For label ["+this.knowledgeBase[i][0]+"], index#"+i+", extracted feature index#"+ft+" was: "+this.knowledgeBase[i][ft]);
+	public void gatherKnowledge(DataExtractor extractor, String mode){
+		//Mode - are we reading into the proper database, or are we reading into our matchbase?
+		if (mode.equals("database")){
+			//Run through everything.
+			for(int i=0; i<size; i++){
+				for(int ft=0; ft<17; ft++){
+					//If our feature is equal zero, i.e. it's the index, gather it.
+					if (ft==0) this.knowledgeBase[i][ft] = extractor.extractLabel(i);
+					//If our feature has an index (1,2,3,4,5,6), extract the feature of that index.
+					else this.knowledgeBase[i][ft] = extractor.extractFeature(i, ft);
+					
+					System.out.println("For label ["+this.knowledgeBase[i][0]+"], index#"+i+", extracted feature index#"+ft+" was: "+this.knowledgeBase[i][ft]);
+				}
 			}
 		}
-		
+		if (mode.equals("matchbase")){
+			//We sorta just copied the above but changed a few things to make it work.
+			for(int i=0; i<sizeMatch; i++){
+				for(int ft=0; ft<17; ft++){
+					//If our feature is equal zero, i.e. it's the index, gather it.
+					if (ft==0) this.matchBase[i][ft] = extractor.extractLabel(i);
+					//If our feature has an index (1,2,3,4,5,6), extract the feature of that index.
+					else this.matchBase[i][ft] = extractor.extractFeature(i, ft);
+					
+					System.out.println("For label ["+this.matchBase[i][0]+"], index#"+i+", extracted feature index#"+ft+" was: "+this.matchBase[i][ft]);
+				}
+			}
+		}		
 	}
+	
+	//testNumbers - using the k-nearest neighbours function, we'll try to give every set of features from the matchbase it's own
+	//custom ID based on what we know in the knowledgebase. Afterwards, we'll match the given ID with the read ID and verify if it's correct.
+	//NOTICE: INVOKE THIS FUNCTION -ONLY- AFTER BOTH DATABASES HAVE BEEN FILLED, OTHERWISE  YOU WILL ONLY GET GIBBERISH!!
+	public void testNumbers(int k){
+		int successValue = 0; //<-- we'll increment this each time we get a correct result.
+		
+		//Fly through the entire matchBase, going through each one.
+		for (int i=0; i<this.sizeMatch; i++){
+			double[][] distance = new double[this.size][1];
+			//For each one, calculate the relative distance between itself and EVERY member of the knowledgebase.
+			//We're using the MANHATTAN METRIC to calculate the distance (aka. pl. "Metryka Uliczna").
+			for(int s=0; s<this.size; s++){
+				double dstVal = 0;
+				for(int ft=1; ft<17; ft++){
+					dstVal+=Math.abs(this.matchBase[i][ft] - this.knowledgeBase[s][ft]);
+				}
+				distance[s][0] = dstVal;
+				distance[s][1] = this.knowledgeBase[s][0];
+			}
+			
+			//When we have our distances, we sort them by size, from the closest to the farthest.
+			java.util.Arrays.sort(distance, new java.util.Comparator<double[]>() {
+			    public int compare(double[] a, double[] b) {
+			        return Double.compare(a[0], b[0]);
+			    }
+			});
+			//And we pick the k closest ones.			
+			//Be democratic. Count the number of each labels appearing.
+			double[] labels = new double[9];
+			for(int clr=0;clr<9;clr++){
+				labels[clr]=0;
+			}
+			for(int pick=0; pick<k; pick++){
+				labels[(int)distance[pick][1]] += 1;
+			}
+			
+			//And assign the most common label.
+			Arrays.sort(labels);
+			//Now check if the label is the same as the label we know is true.
+		}
+		
+		
+		//All done? Let's check our results:
+		double success = ((double)successValue / (double)this.sizeMatch) * 100;
+		System.out.println("We're done. Our effective guess success rate measures at: "+success+"%!");
+	}
+	
 	//saveKnowledge - this function will write a simple text file with the entire knowledgebase printed inside of it.
 	//The file lacks any sort of special and funky formatting since it's always set for 60k iterations of a 4-D matrix formula.
 	public void saveKnowledge(String filename) throws IOException{
@@ -84,7 +148,6 @@ public class Knowledge {
 	
 	//readKnowledge - this function will take our text file generated by the above and attempt to read it and fill out the respective values.
 	//This function is a bit more advanced since we have to interpet the string values as doubles, also fill them in the apporpriate places.
-	
 	public void readKnowledge(String filename) throws IOException{
 		File file = new File(filename);
 		Scanner scan = new Scanner(file);
