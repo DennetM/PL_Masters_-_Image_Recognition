f4dc70c45791c17285e98f6cea514879ab002dcb
DennetM <DennetM.alt@gmail.com>
2015-12-09 13:39:19 +0100

    Imported the Apache Commons Math library.
    It's required for FFT, as before. You can see it's addition to gitignore
    file because we don't want to bloat our servers with 50 MB of crap.

---
+++ Zadanie 2/ocrRecognizer/FFT.java
@@ -0,0 +1,17 @@
+package ocrRecognizer;
+
+//Hello darkness my old friend.
+//HIGHLY REPURPOSED FROM OUR OLD FOURIER TRANSFORM PROGRAM.
+public class FFT {
+	
+	//Variables - none.
+	
+	//Constructor - none.
+	
+	//Functions - a few. The main function is in PublicStaticLand so we're just gonna use this class as a container.
+	public static double[][] FFT(String Direction, int[][] image){
+		
+		return null;
+	}
+
+}
