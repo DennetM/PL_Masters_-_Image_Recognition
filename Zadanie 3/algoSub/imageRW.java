030382e0a7f3de8ad2277fb01387c872fd8a0401
DennetM <DennetM.alt@gmail.com>
2016-01-25 10:26:56 +0100

    Late Reading and Sector Expansion Commit
    You know what this entails, it's nothing difficult.

--- Zadanie 3/algoSub/imageRW.java
+++ Zadanie 3/algoSub/imageRW.java
@@ -1,5 +1,33 @@
 package algoSub;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
 public class imageRW {
+	
+	//No constructors or variables - PUBLIC STATIC LAND~!
+	
+	//readImage - reads the image into a "3D" table.
+	//The table's first column is the channel switch. 0 - red, 1 - green, 2 - blue.
+	public static int[][][] readImage(String type, String filename) throws IOException{
+		String path = type+"/"+filename;
+		BufferedImage img = ImageIO.read(new File(path));
+		int[][][] intImg = new int[3][img.getWidth()][img.getHeight()];
+		
+		for(int x=0; x<img.getWidth(); x++){
+			for(int y=0; y<img.getHeight(); y++){
+				Color col = new Color(img.getRGB(x, y));
+				intImg[0][x][y] = col.getRed();
+				intImg[1][x][y] = col.getGreen();
+				intImg[2][x][y] = col.getBlue();
+			}
+		}
+		return intImg;
+	}
 
 }
--- Zadanie 3/algoSub/regionSelector.java
+++ Zadanie 3/algoSub/regionSelector.java
@@ -2,4 +2,26 @@ package algoSub;
 
 public class regionSelector {
 
+	//Variables.
+	private boolean[][] checkMap; //This map will remember which pixels we've already run through.
+	
+	//Constructor - each selector is tied to only one image!
+	public regionSelector(int sizeX, int sizeY){
+		this.checkMap = new boolean[sizeX][sizeY];
+		for(int x=0; x<sizeX; x++){
+			for(int y=0; y<sizeY; y++){
+				this.checkMap[sizeX][sizeY] = false;
+			}
+		}
+	}
+	
+	//Functions.
+	//Gets, since we're not setting anything in this class.
+	public boolean[][] getMap(){ return this.checkMap; }
+	public boolean checkXY(int x, int y){ return this.checkMap[x][y]; }
+	
+	//markRegion - taking in the seed x and y, as well as a threshold value, this function updates the checkMap.
+	public void markRegion(int seedX, int seedY, double threshold, int[][][] img){
+		//TODO:EVERYTHING
+	}
 }
