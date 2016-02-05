bb9dbf99a6faea9705d6513ebad56b8994c3758f
paulabee <billbort25@interia.pl>
2016-02-05 18:30:13 +0100

    Back to square one...
    Java Black Magics and Witchcraft

--- Zadanie 3/main.java
+++ Zadanie 3/main.java
@@ -8,7 +8,6 @@ public class main {
 
 	public static void main(String[] args) throws IOException{
 		int[][][] img = imageRW.readImage("Wino", "count1.bmp");
-		//int[][][] img = imageRW.readImage("Wino", "F://Java Projects//PL_Masters_-_Image_Recognition//Zadanie 3//images//count1.bmp");
 		
 		detectGrape test = new detectGrape(img, 43, 43);
 		imageRW.checkMapSave(test.checkMap, 800, 600, "testCheck.png");
